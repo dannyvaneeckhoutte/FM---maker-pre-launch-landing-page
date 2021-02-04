53eef5d9eff95b1d70862afd980fb2e40833516f
Danny <danny.vaneeckhoutte@gmail.com>
2021-02-04 21:45:31 +0100

    email validation

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var emailField = document.getElementById("emailField");
+var button = document.getElementById("button");
+var response = document.getElementById("response");
+
+button.addEventListener("click", function() {
+    var email = emailField.value;
+    if (validateEmail(email)) {
+        response.innerHTML = "Hiya Cowboy, this email will work for us 🤠";
+    } else {
+        response.innerHTML = "Sorry, this email is not cool enough 😩";
+    }
+});
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
